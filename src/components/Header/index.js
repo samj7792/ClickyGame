01173561ced789d9bd978cc5a4925b2237cffad0
Sam Jackson <samuelsjackson7@gmail.com>
2019-08-15 13:24:58 -0400

    added some styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,11 +3,19 @@ import React from 'react';
 function Header({score, wins, losses}) {
   return (
     <div>
-      <div className='jumbotron'>
-          <h1>Clicky Game</h1>
-          <h3>Score: {score}</h3>
-          <h3>Wins: {wins}</h3>
-          <h3>Losses: {losses}</h3>
+      <div className='jumbotron bg-info'>
+        <div className="row">
+          <div className="col-4">
+            <h1>Clicky Game</h1>
+          </div>
+          <div className="col-4">
+          </div>
+          <div className="col-4 text-right">
+            <h3>Score: {score}</h3>
+            <h3>Wins: {wins}</h3>
+            <h3>Losses: {losses}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
