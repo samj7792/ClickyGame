aac3180fea71af8911d2a30647c196403995f42a
Sam Jackson <samuelsjackson7@gmail.com>
2019-08-15 13:51:38 -0400

    added mobile responsiveness to columns

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Counter (props) {
   return (
-    <div className='col-2'>
+    <div className='col-md-2 col-sm-3 col-4'>
           <div className="card mt-4">
             <div className="img-container">
               <img className="card-img" src={props.src} alt={props.alt} id={props.id} onClick={props.onClick} />
