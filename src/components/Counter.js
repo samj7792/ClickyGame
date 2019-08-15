b78c1ba881e944f886a75709c7261ee9a1775965
Sam Jackson <samuelsjackson7@gmail.com>
2019-08-15 13:54:49 -0400

    fixed some tabbing in Counter.js

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,10 +3,10 @@ import React from "react";
 function Counter (props) {
   return (
     <div className='col-md-2 col-sm-3 col-4'>
-          <div className="card mt-4">
-            <div className="img-container">
-              <img className="card-img" src={props.src} alt={props.alt} id={props.id} onClick={props.onClick} />
-            </div>
+      <div className="card mt-4">
+        <div className="img-container">
+          <img className="card-img" src={props.src} alt={props.alt} id={props.id} onClick={props.onClick} />
+        </div>
       </div>
     </div>
   );
